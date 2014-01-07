4e200ecbf96ae00ba67b2a7915c1b4ff7ac9e58a
Shannon Lewis <slewis74@gmail.com>
2014-01-08 09:05:08 +1000

    updated to use the 8.1 SettingsFlyout

--- SlabRt/Pages/Navigation/RtNavigator.cs
+++ SlabRt/Pages/Navigation/RtNavigator.cs
@@ -2,25 +2,15 @@
 using Slab.Pages;
 using Slab.Pages.Navigation;
 using Slab.PresentationBus;
-using SlabRt.Pages.Settings;
-using Windows.UI;
 using Windows.UI.ApplicationSettings;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
 
 namespace SlabRt.Pages.Navigation
 {
     public class RtNavigator : Navigator, IRtNavigator
     {
-        private Popup _settingsPopup;
-        private readonly int _settingsWidth;
-
         public RtNavigator(IPresentationBus presentationBus, IControllerInvoker controllerInvoker) : base(presentationBus, controllerInvoker)
         {
-            _settingsWidth = 346;
         }
 
         protected override void DoNavigate(ControllerInvokerResult controllerResult)
@@ -29,7 +19,7 @@ namespace SlabRt.Pages.Navigation
             var settingsResult = result as ISettingsPageActionResult;
             if (settingsResult != null)
             {
-                DoPopup(settingsResult);
+                DoSettingsPopup(settingsResult);
                 return;
             }
 
@@ -38,77 +28,16 @@ namespace SlabRt.Pages.Navigation
 
         public void SettingsNavigateBack()
         {
-            if (_settingsPopup != null)
-            {
-                _settingsPopup.IsOpen = false;
-            }
-
             // the back button shows the Settings pane again.
             SettingsPane.Show();
         }
 
-        private void DoPopup(ISettingsPageActionResult settingsResult)
+        private void DoSettingsPopup(ISettingsPageActionResult settingsResult)
         {
-            var windowBounds = Window.Current.Bounds;
-
-            // Create a Popup window which will contain our flyout.
-            _settingsPopup = new Popup();
-            _settingsPopup.Closed += OnPopupClosed;
-
-            Window.Current.Activated += OnWindowActivated;
-
-            _settingsPopup.IsLightDismissEnabled = true;
-
-            _settingsPopup.Width = _settingsWidth;
-            _settingsPopup.Height = windowBounds.Height;
-
-            // Add the proper animation for the panel.
-            _settingsPopup.ChildTransitions = new TransitionCollection
-                                                  {
-                                                      new PaneThemeTransition
-                                                          {
-                                                              Edge = (SettingsPane.Edge == SettingsEdgeLocation.Right)
-                                                                         ? EdgeTransitionLocation.Right
-                                                                         : EdgeTransitionLocation.Left
-                                                          }
-                                                  };
-
-            // Create a SettingsFlyout the same dimenssions as the Popup.
-            var view = (FrameworkElement)Activator.CreateInstance(settingsResult.PageType);
+            var view = (SettingsFlyout)Activator.CreateInstance(settingsResult.PageType);
             view.DataContext = settingsResult.Parameter;
 
-            view.Width = _settingsWidth;
-            view.Height = windowBounds.Height;
-
-            // Place the SettingsFlyout inside our Popup window.
-            var settingsPopupViewModel = new SettingsPopupViewModel(new SettingsBackCommand(this));
-
-            _settingsPopup.Child = new SettingsPopupView
-            {
-                DataContext = settingsPopupViewModel,
-                HeaderBackground = new SolidColorBrush(Colors.Green),
-                Background = new SolidColorBrush(Colors.LightGreen),
-                Content = view
-            };
-
-            // Let's define the location of our Popup.
-            _settingsPopup.SetValue(Canvas.LeftProperty, SettingsPane.Edge == SettingsEdgeLocation.Right ? (windowBounds.Width - _settingsWidth) : 0);
-            _settingsPopup.SetValue(Canvas.TopProperty, 0);
-            _settingsPopup.IsOpen = true;
+            view.Show();
         }
-
-        private void OnWindowActivated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
-        {
-            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
-            {
-                _settingsPopup.IsOpen = false;
-            }
-        }
-
-        void OnPopupClosed(object sender, object e)
-        {
-            Window.Current.Activated -= OnWindowActivated;
-        }
-
     }
 }
