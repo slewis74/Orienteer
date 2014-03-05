f989d1605aae94b4964a17f987b87cdbb58ae42d
slewis <slewis74@gmail.com>
2014-03-05 17:45:31 +1000

    don't need Configure in the interface

--- SlabRt/Pages/IViewLocator.cs
+++ SlabRt/Pages/IViewLocator.cs
@@ -5,8 +5,6 @@ namespace SlabRt.Pages
 {
     public interface IViewLocator
     {
-        void Configure(Assembly assembly, string baseViewModelNamespace, string baseViewNamespace);
-
         FrameworkElement Resolve(object viewModel, PageLayout pageLayout);
     }
 }
