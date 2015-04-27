1298bd8f04d7b8d2626ecb8a75c4ebf3ca839b10
slewis <slewis74@gmail.com>
2015-04-28 08:25:33 +1000

    Added Xamarin Forms project

---
+++ Slab.Forms/Pages/ViewLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Slab.Xaml;
+using Xamarin.Forms;
+
+namespace Slab.Forms.Pages
+{
+    public class ViewLocator : ViewLocatorBase<Page>, IViewLocator
+    {
+        public override Page Resolve(object viewModel)
+        {
+            var viewType = DetermineViewType(viewModel.GetType());
+            var view = (Page)Activator.CreateInstance(viewType);
+            view.BindingContext = viewModel;
+            return view;
+        }
+    }
+
+    public interface IViewLocator : IViewLocator<Page>
+    { }
+}
